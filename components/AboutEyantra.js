5c4ce7f653f61ec6ad46831eb0ffd4ffcdc102d8
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2022-02-05 21:51:19 +0530

    seventh commit

--- components/AboutEyantra.js
+++ components/AboutEyantra.js
@@ -5,7 +5,7 @@ function AboutEyantra() {
   <div>
       <div style={{  marginRight : 70, paddingBottom : 40, color : "#fff", paddingTop : 30,backgroundColor : "#000",width : "100vw"}} >   
       <div style={{padding : 0,padding: "0 4rem", paddingTop : 20,paddingBottom : 50,backgroundColor : "#000"}}>
-          <p style={{fontSize : 26, margin : 0, lineHeight : 1, marginTop : 30, color : "#fff"}}>About E-yantra ?</p>
+          <p style={{fontSize : 26, margin : 0, lineHeight : 1, marginTop : 30, color : "#fff"}}>What is eYRC - e-Yantra Robotics Competition ?</p>
           <p style={{fontSize : 14, margin : 0, lineHeight : 2, color : "#fff",fontWeight : 400, marginTop : 10}}>
             <li> A truly inclusive and international competition accommodating teams with varied level of skills from beginners to advanced teams.</li>
             <li>Our abstracted gamified problem statements (called Themes) range from a variety of skills including ROS, Machine Learning, Image Processing, 3D Designing, Embedded Systems, Functional Programming and much more.</li>
